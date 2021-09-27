8788bd176a9d5c874fc05b7e1556449fea83f4eb
ArulSeenivasan <johnkovilur123@gmail.com>
2021-09-26 20:06:57 -0700

    After code correction

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("It is a third Program");
-		System.out.printf("I had some Modification done");
-		Symtem.out.printf("Second Chnages");
+		System.out.printf("It is a third Program\n");
+		System.out.printf("I had some Modification done\n");
+		System.out.printf("Second Chnages\n");
 	}
 		
 }
